9677bce1dcbda94b547a10ed97212086c5edb19d
JpythonS <jpgoncalves1296@gmail.com>
2024-10-20 18:10:19 -0300

    added lazy loading for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
+import { Box, Center, Spinner } from "@chakra-ui/react";
+import { Suspense, lazy } from "react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/home/HomePage';
-import RoomsPage from './pages/rooms/RoomsPage';
-import NotFoundPage from './pages/not-found/NotFoundPage';
-import WaitingRoomPage from './pages/waiting-room/WaitingRoomPage';
-import GameRoomPage from './pages/game-room/GameRoomPage';
+
+const HomePage = lazy(() => import('./pages/home/HomePage'));
+const RoomsPage = lazy(() => import('./pages/rooms/RoomsPage'));
+const NotFoundPage = lazy(() => import('./pages/not-found/NotFoundPage'));
+const WaitingRoomPage = lazy(() => import('./pages/waiting-room/WaitingRoomPage'));
+const GameRoomPage = lazy(() => import('./pages/game-room/GameRoomPage'));
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/rooms" element={<RoomsPage />} />
-        <Route path="/waiting/:roomId" element={<WaitingRoomPage />} />{' '}
-        <Route path="/game/:roomId" element={<GameRoomPage />} />{' '}
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={
+        <Center height="100vh">
+          <Box>
+            <Spinner size="xl" />
+            <Box mt={4}>Loading...</Box>
+          </Box>
+        </Center>
+      }>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/rooms" element={<RoomsPage />} />
+          <Route path="/waiting/:roomId" element={<WaitingRoomPage />} />{' '}
+          <Route path="/game/:roomId" element={<GameRoomPage />} />{' '}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
